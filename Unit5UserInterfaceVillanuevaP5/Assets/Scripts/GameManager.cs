4a261942eef81cfe284a38eed1df24be1293aa66
ZoopSoup <zoopidysoupidy@gmail.com>
2023-05-01 11:41:48 -0700

    I added upon my GameManager and Aim
    w2dwdw

--- Unit5UserInterfaceVillanuevaP5/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceVillanuevaP5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
+        isGameActive = true;
     }
 
     // Update is called once per frame
@@ -21,12 +30,24 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
 
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
