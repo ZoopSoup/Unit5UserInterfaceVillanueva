5489cdf5b5d0317c78860be775bb199e3de8e2c6
ZoopSoup <zoopidysoupidy@gmail.com>
2023-05-03 11:50:31 -0700

    DONE!!!!
    WOOHOOOOOOOOOOOOO

--- Unit5UserInterfaceVillanuevaP5/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceVillanuevaP5/Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
@@ -15,11 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
 
-        isGameActive = true;
     }
 
     // Update is called once per frame
@@ -47,7 +47,25 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        spawnRate /= difficulty;
+
+        isGameActive = true;
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
